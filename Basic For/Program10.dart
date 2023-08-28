01aadc35495d3cea05869e7a92045567ea12750f
shivraje chaudhari <121508292+Devill7575@users.noreply.github.com>
2023-08-28 10:21:10 +0530

    Add files via upload

---
+++ Basic For/Program10.dart
@@ -0,0 +1,7 @@
+void main() {
+  int mult = 1;
+  for(int i=10;i>=1; i--){
+    mult = mult*i;
+  }
+  print(mult);
+}
---
+++ Basic For/Program9.dart
@@ -0,0 +1,7 @@
+void main() {
+  int sum = 0;
+  for(int i=10;i>=1; i--){
+    sum = sum+i;
+  }
+  print(sum);
+}
